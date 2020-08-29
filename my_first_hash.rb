e0d2903debc5b0ccc2cc6d5d1e2bc5c27f77bd1c
Chris Houghton <chrishoughton2434@gmail.com>
2020-08-29 13:33:16 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -9,7 +9,6 @@ def shipping_manifest
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  shipping_manifest["oil paintings"]
 end
 
 def retrieval
