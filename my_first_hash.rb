521b0bd13d661857c91b104020c7c2df6b845117
Chris Houghton <chrishoughton2434@gmail.com>
2020-08-29 13:58:55 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -26,6 +26,6 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-    shipping_manifest.merge("gun powder" => 4, "muskets" => 2)
+    moby_dick = shipping_manifest.merge("gun powder" => 4, "muskets" => 2)
   return shipping_manifest
 end
