743ca4fd2cbce63b712292af5bda8dcb8cb59a81
Chris Houghton <chrishoughton2434@gmail.com>
2020-08-29 13:55:29 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -26,7 +26,7 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
- shipping_manifest.merge("gun powder" => 4)
+ shipping_manifest.merge("gun powder" => 4, "muskets" => 2)
 
 
   # add 4 gun powder to the shipping_manifest hash below
